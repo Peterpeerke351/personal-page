726af95fb2b06a4a79aa797aad5cb999b5c6cc45
Peter <48333532+Peterpeerke351@users.noreply.github.com>
2020-04-24 23:23:49 +0200

    begin

---
+++ Include/Head.php
@@ -0,0 +1,17 @@
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- CSS file -->
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
+
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css?family=Lobster|Pacifico&display=swap" rel="stylesheet">
+
+  <title>PtrvanDongen</title>
+
+</head>
--- Index.php
+++ Index.php
@@ -1,2 +1,15 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
+<?php include 'Include/Head.php';?>
+
+<body>
+
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+
+<?php include 'Include/footer.php';?>
+
+</body>
+</html>
